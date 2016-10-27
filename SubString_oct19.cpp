b5993e08a9aca9d8c23ca115cc39e8b92209a1e6
Chandani Shrestha <chandani@Chandanis-MacBook-Air.local>
2016-10-26 23:04:21 -0400

    Fixed code to find substring.

--- SubString_oct19.cpp
+++ SubString_oct19.cpp
@@ -2,39 +2,55 @@
 #include <string>
 using namespace std;
 
-bool check_string(string sentence, string sentenceSubstr)
+bool check_string(string main_string, string sub_string)
 {
-    int length = sentence.size(), length_string = sentenceSubstr.size();
-    bool flag = false;
-    
-    for(int i = 0, j = 0; i < length; i++)
+    int length_mainString = main_string.size();
+    int length_subString = sub_string.size();
+    int count = 0;
+
+    for(int i = 0; i < length_mainString; i++)
     {
-        if (j >= length_string)
-            break;
-        if (sentenceSubstr[j] == sentence[i])
-        {    
-            j++;
-            flag = true;
-        }      
-        else
-            flag = false;
-    } 
-    
-    return flag;
+        if(sub_string[0] == main_string[i])
+        {
+            for(int j = 1; j < length_subString; j++)
+            {
+                if(sub_string[j] == main_string[i+1])
+                {
+                    count++;
+                    i++;
+                }   
+                else
+                {
+                    i++;
+                    break;
+                }
+
+            }
+            if(count == length_subString - 1)
+                return true;
+        }
+        
+    }
+    return false;
 }
-    
+
+
+
 
 int main()
 {
-    string sentence, sentenceSubstr;
-  
-    cout << "Enter the Sentence: " << endl;
-    getline (cin, sentence);
-    cout << "Enter the substring you want to find: " << endl;
-    getline (cin, sentenceSubstr);
-    
-    if (check_string(sentence, sentenceSubstr) == true)
+    string main_string, sub_string;
+
+    cout << "Enter a string: " << endl;
+    getline(cin, main_string);
+
+    cout << "Enter the substring you want to search: " << endl;
+    getline(cin, sub_string);
+
+    if(check_string(main_string, sub_string))
         cout << "Found" << endl;
     else
         cout << "Not found" << endl;
+
+    return 0;
 }
