4f8414c142188e43c84579318f53c625379355da
Chandani Shrestha <chandani@Chandanis-MacBook-Air.local>
2016-11-20 16:09:07 -0500

    Displaying Fibonacci Series without recursion

--- FibWithoutRecursion.cpp
+++ FibWithoutRecursion.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void fibonacci(int fibonacci_num) 
+void fibonacci(int num) 
 {   
     int n1 = 0, n2 = 1, n3 = 1;   
-    while (fibonacci_num > 0)
+    for(int i = 0; i < num; i++)
     {
         cout << n3 << endl; // "1" needs to be printed twice
         n3 = n2 + n1; //general formula for fibonacci series
         n1 = n2;       
         n2 = n3;
-        fibonacci_num--; 
     }
 }
 
 int main() 
 {
-    int fibonacci_num;
+    int num;
     cout << "Enter the number of fibonacci you want to display: " ;
-    cin >> fibonacci_num;
-    cout << "Fibonacci series of " << fibonacci_num << " numbers:" << endl;
-    fibonacci(fibonacci_num);
+    cin >> num;
+    cout << "Fibonacci series of " << num << " numbers:" << endl;
+    fibonacci(num);
 }
