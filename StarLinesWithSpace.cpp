45f04ed9c4cfdaf38bbdb14a8bf553d183cf602b
Chandani Shrestha <chandani@vt.edu>
2016-12-20 21:13:20 -0500

    Print Lines with starsand space

---
+++ StarLinesWithSpace.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout << "Enter the number of lines: ";
+	cin >> num;
+	for (int i = 1; i <= num; i++)
+	{
+		for (int j = num; j > i; j--)	
+		{
+			cout << " ";
+		}
+		for (int j = 1; j <= i; j++)	
+		{
+			cout << "*" ;
+		}
+
+		cout << endl;
+	}
+return 0;
+}
