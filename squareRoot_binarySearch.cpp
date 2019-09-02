e424f25efdd3cc853865017a10c650beffb1c51a
Chandani Shrestha <chandani@vt.edu>
2019-09-02 16:40:16 -0400

    Truncated square root using binary search

--- squareRoot_binarySearch.cpp
+++ squareRoot_binarySearch.cpp
@@ -2,19 +2,25 @@
 using namespace std;
 
 int findSquareRoot(int x){
-	int start = 0;
-		while (start <= x ){
-			if (start * start  == x)
-				return start;
-			else if (start * start < x)
-			{
-				start++;
-			}
-			else 
-				return start-1;
-		}
-		return -1;
-}
+	int start = 1;
+    int end = x/2;
+    int temp = -1;
+    if (x == 0 || x == 1 )
+        return x;
+    while (start <= end){
+        double midVal = (start +end) / 2;
+        if (midVal * midVal == x)
+            return midVal;
+        if (midVal * midVal < x){
+            start = midVal + 1;
+            temp = midVal;
+        }
+        else
+            end = midVal - 1;
+            
+        } 
+        return temp;    
+    }
 
 int main(){
 	int rootX;
